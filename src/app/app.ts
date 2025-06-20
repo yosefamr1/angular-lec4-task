e95d4e25794947f34eb36eea67a7a5ef6c5f939c
YoussefTrust <yosefamr2002@icloud.com>
2025-06-20 21:03:18 +0300

    repo ready to code now

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Cards } from "./cards/cards";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Cards],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
